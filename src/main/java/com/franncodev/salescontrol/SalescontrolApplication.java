ad00c72737e722f122ba6e9aede1c609aa3ba628
frannco-dev <195823139+frannco-dev@users.noreply.github.com>
2025-06-05 12:07:38 -0300

    Add initial project structure

---
+++ src/main/java/com/franncodev/salescontrol/SalescontrolApplication.java
@@ -0,0 +1,13 @@
+package com.franncodev.salescontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalescontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalescontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franncodev/salescontrol/SalescontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franncodev.salescontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalescontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
